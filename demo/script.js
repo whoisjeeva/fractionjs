5c922152b2c361fa9cb24e07d9579152cf93ac75
anyms <mytellee@gmail.com>
2019-05-06 21:26:07 +1000

    deleting old test files

--- demo/script.js
+++
@@ -1,9 +0,0 @@
-var h1 = document.getElementsByTagName('h1')[0];
-var num1 = 7, num2 = 15;
-
-var jmath = new jMath(String(num1));
-jmath.pow(2);
-
-h1.innerHTML = "jMath: " + jmath.value;
-
-console.log();
